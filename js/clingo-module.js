682b71cec8ffcd309ccd91250fec023d770a5bc0
Ronald de Haan <me@ronalddehaan.eu>
2024-10-30 22:44:31 +0100

    Small update

--- js/clingo-module.js
+++ js/clingo-module.js
@@ -203,7 +203,9 @@ function check_if_stratified_and_simple(program) {
   console.log(reified_program);
   check_program = stored_programs["stratified"];
   answer_set = get_answer_set(reified_program + check_program);
+  console.log(reified_program + check_program);
   if (answer_set) {
+    console.log(answer_set);
     answer_set = filter_answer_set(answer_set, ["has_choice","has_proper_disjunction","has_negative_cycle"]);
     return answer_set.length == 0;
   }
