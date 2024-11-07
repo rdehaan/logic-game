3e972ac15c2de30669d9ff38736367b1d80430a2
Ronald de Haan <me@ronalddehaan.eu>
2024-11-07 11:17:52 +0100

    Small update

--- js/control.js
+++ js/control.js
@@ -240,16 +240,3 @@ function save(filename, data) {
         document.body.removeChild(elem);
     }
 }
-
-// var param1var = getQueryVariable("param1");
-// function getQueryVariable(variable) {
-//   var query = window.location.search.substring(1);
-//   var vars = query.split("&");
-//   for (var i=0;i<vars.length;i++) {
-//     var pair = vars[i].split("=");
-//     if (pair[0] == variable) {
-//       return pair[1];
-//     }
-//   }
-//   return null;
-// }
--- js/init.js
+++ js/init.js
@@ -2,3 +2,37 @@ function initalize_after_clingo_loaded() {
   load_game_from_path('examples/flag1.json');
   lock_level_generation();
 };
+
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      return pair[1];
+    }
+  }
+  return null;
+}
+
+// Only show (explicit) level editor if specified in the URL
+if (getQueryVariable("editor") != "true") {
+  document.getElementById('level-editor').style.display = "none";
+}
+
+// Select a particular tab if specified in the URL
+if (getQueryVariable("tab") == "1") {
+  document.getElementById('tab1').checked = true;
+}
+if (getQueryVariable("tab") == "2") {
+  document.getElementById('tab2').checked = true;
+}
+if (getQueryVariable("tab") == "3") {
+  document.getElementById('tab3').checked = true;
+}
+if (getQueryVariable("tab") == "4") {
+  document.getElementById('tab4').checked = true;
+}
+if (getQueryVariable("tab") == "5") {
+  document.getElementById('tab5').checked = true;
+}
