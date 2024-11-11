cea82a931be51d05beb9e38bd57fe54b79c9e9ce
Ronald de Haan <me@ronalddehaan.eu>
2024-11-11 10:06:54 +0100

    Small update

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 function initalize_after_clingo_loaded() {
-  load_game_from_path('examples/flag1.json');
+  load_game_from_path('examples/flag1-with-solutions.json');
   lock_level_generation();
 };
 
