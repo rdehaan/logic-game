eea0ccee0ce89f8ac588a9c8662ac4b9a41697c3
Ronald de Haan <me@ronalddehaan.eu>
2025-07-25 10:33:51 +0200

    Add debugging in js console

--- js/game.js
+++ js/game.js
@@ -222,6 +222,8 @@ function generate_player_move(working_game, player_input, player_memory) {
     player_moves = answer_set_to_facts(player_moves);
     var memory_updates = filter_answer_set(answer_set, ["remember","forget"]);
     memory_updates = answer_set_to_facts(memory_updates);
+    console.log("MEM UPDATES")
+    console.log(memory_updates)
     return {
       player_moves: player_moves,
       memory_updates: memory_updates
